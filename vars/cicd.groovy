9d51d4208c9078db2ccab82833c9d330991790cf
vidyasrees <119058981+vidyasrees@users.noreply.github.com>
2024-04-26 15:01:01 +0530

    Update cicd.groovy

--- vars/cicd.groovy
+++ vars/cicd.groovy
@@ -6,4 +6,8 @@ def build()
 {
   sh "mvn package"
 }
+def deploy(jobname,ip,appname)
+{
+  sh "scp /var/lib/jenkins/workspace/${jobname}/webapp/target/webapp.war ubuntu@${ip}:/var/lib/tomcat9/webapps/${appname}.war"
+}
 
